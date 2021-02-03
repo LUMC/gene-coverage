9fef6918a6a1382fec0ca6618d3639664fc550b8
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-02-03 14:30:30 +0100

    Refactor

--- src/gene_coverage/coverage.py
+++ src/gene_coverage/coverage.py
@@ -13,21 +13,22 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with gene-coverage.  If not, see <https://www.gnu.org/licenses/
-from typing import NamedTuple, Generator
+from typing import Generator
 
 from pybedtools.bedtool import BedTool
-from pybedtools.bedtool import Interval
-from vtools.gcoverage import Region as OneBasedRegion
 from vtools.gcoverage import RefRecord
 
 
-def refflat_to_bedtools(filename: str) -> Generator[BedTool, None, None]:
+def file_to_refflat_records(filename: str) -> Generator[RefRecord, None, None]:
     with open(filename, "rt") as file_h:
         for line in file_h:
-            ref_record = RefRecord.from_line(line)
-            bed_regions = ((
-                f"{exon.chr}\t{exon.start}\t{exon.end}\t"
-                f"{ref_record.transcript}\t.\t"  # name, score
-                f"{'+' if ref_record.forward else '-'}\n")  # strand
-                for exon in ref_record.exons)
-            yield BedTool("".join(bed_regions), from_string=True)
+            yield RefRecord.from_line(line)
+
+
+def refflat_record_to_bedtool(refflat_record: RefRecord) -> BedTool:
+    bed_regions = ((
+        f"{exon.chr}\t{exon.start}\t{exon.end}\t"
+        f"{refflat_record.transcript}\t.\t"  # name, score
+        f"{'+' if refflat_record.forward else '-'}\n")  # strand
+        for exon in refflat_record.exons)
+    return BedTool("".join(bed_regions), from_string=True)
